e1aa07e0d3f0965d8c2e9a3c4dc1b77cd3aca533
Kiran5258 <skirankumar97849@gmail.com>
2025-03-19 16:15:10 +0530

    Update icon and form

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -16,7 +16,7 @@ export default function EventDetails() {
 ➢Data Science
 ➢Cloud computing
 ➢Current Trends(Relate to IT)`,
-      registerLink: "https://forms.gle/K8vGEMNpxFfcwN1H8",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `❖Maximum number of participants in a team is 3 and the minimum number is 2.
 ❖Abstract should not exceed more than 250 words and paper should not exceed 15 pages.
 ❖Soft copy of the submitted paper must be in .docx format.
@@ -35,7 +35,7 @@ The event will last 40 minutes, with a possible second round if required.
 Participants will design a webpage UI/UX based on a given theme or problem statement.
 Only Figma and Adobe XD are allowed; pre-made templates are not permitted.
 Plagiarism is strictly prohibited—original designs are required.`,
-      registerLink: "https://forms.gle/zAMAyzwwLG11uBDE6",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `●Duo participation only.
 ●The total duration of the event is 40 minutes.
 ●If needed, Round 2 will be conducted based on the competition's requirements
@@ -52,7 +52,7 @@ External assistance and plagiarism are strictly prohibited.
 Each round will have a 30-minute time limit.
 Participants will debug faulty code containing syntax, logical, or runtime errors while maintaining the program’s intended logic.
 A second round will be conducted only if needed in case of ties or close competition.`,
-      registerLink: "https://forms.gle/pMhECC6P9Z49og8m6",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `●Solo participation only – team entries are not allowed.
 ●Any form of external assistance or plagiarism is strictly prohibited.
 ●A second round will be conducted only if needed (e.g., in case of ties or close competition).
@@ -68,7 +68,7 @@ Each level presents a coding, logic, or encryption puzzle that must be solved to
 Failed challenges can be retried, but with potential time penalties.
 Hints are available, but they reduce the final score or add a time penalty.
 The game ends if the timer reaches zero before all challenges are completed.`,
-      registerLink: "https://forms.gle/VcPARECvHhPjfyveA",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `●Duo participation only.
 ●The game must be completed within 60 minutes.
 ●Players must complete each level to unlock the next one.
@@ -81,13 +81,13 @@ The game ends if the timer reaches zero before all challenges are completed.`,
     5: {
       title: "DIGITAL DEATH MATCH",
       description: `The event features two popular battle royale games: BGMI and Free Fire.
-In BGMI, squads must have a minimum of 3 players, and emulators are strictly prohibited.
+In BGMI, squads must have a  4 players, and emulators are strictly prohibited.
 Free Fire matches will be played in Battle Royale Squad Mode, with open qualifiers and finals.
 Players must bring their own mobile devices with an active internet connection.
 Hacking or teaming up will result in immediate disqualification. Winners will be determined based on rank position in BGMI and match results in Free Fire.`,
-      registerLink: "https://forms.gle/kBYJwkZrWf3dW5Ko6",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `BGMI
-●Minimum 3 players should be in a squad.
+●Must have a 4 players should be in a squad.
 ●Emulators are not allowed.
 ●The player should bring their mobile and have an internet connection.
 ●If you are caught hacking or teaming up your team will be disqualified.
@@ -115,12 +115,12 @@ Echoverse:
 ❖They can either recreate the original dialogue or create their own version.
 ROUND 4 :
 Miccheck:
-❖ Participants will receive a random topic on the spot.
+❖ Participants will receive a random/ topic on the spot.
 ❖ The speech should not exceed more than 2 minutes.
 ❖ Irrelevant content will result in disqualification.
 ❖ Speech can be in Tamil or English.
  One minute will be given for preparation.`,
-      registerLink: "https://forms.gle/UD16w2EWExfYTJaR9",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `●Limited participant's  only .first-come, first-served basis
 ●The use of mobile phones or any other devices is strictly prohibited.
 ●Only solo participation is allowed.
@@ -135,7 +135,7 @@ Online registration is mandatory; no offline entries will be accepted.
 Teams will engage in a thrilling auction-style competition, strategizing to build the best squad.
 Only two prizes will be awarded: Winner & Runner-up.`,
       note: "Note: first come first serve",
-      registerLink: "https://forms.gle/wgeX1ir2amUiBRet7",
+      registerLink: "https://forms.gle/Wx8pU2E27qkjazCw9",
       Rule: `●Maximum 10 teams to be selected based on registration.
 ●First Come First Serve
 ●Maximum 4 participants per team.
--- src/components/Motion.jsx
+++ src/components/Motion.jsx
@@ -4,24 +4,61 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import bgvideo from "../assets/Bgvideo.mp4";
 import { useNavigate } from "react-router-dom";
-import ipl from "../assets/456.webp"
-import front from "../assets/Frontman.webp"
-import paperpresent from "../assets/paperpresentation.webp"
+// import krcelogo from "../assets/11.png";
+import ipl from "../assets/456.webp";
+import front from "../assets/Frontman.webp";
+import paperpresent from "../assets/paperpresentation.webp";
 import uiux from "../assets/uiux.webp";
 import Escape from "../assets/Escape.webp";
 import debug from "../assets/debugging.webp";
 import mobliegame from "../assets/mobliegame.webp";
+import bg from "../assets/bg2.jpg"
 export default function Motion() {
   const navigate = useNavigate();
 
   const events = [
-    { id: "1", title: "DALGONA PAPERS", description: "PAPER PRESENTATION", image: `${paperpresent}` },
-    { id: "2", title: "GLASS BRIDGE DESIGN", description: "UI/UX", image: `${uiux}` },
-    { id: "3", title: "THE MASKED BUG", description: "DEBUGGING", image: `${debug}` },
-    { id: "4", title: "THE ELIMINATION ROOM", description: "THE GLITCH ESCAPE", image: `${Escape}` },
-    { id: "5", title: "DIGITAL DEATH MATCH", description: "Mobile Games", image: `${mobliegame}`},
-    { id: "6", title: "THE FRONT MAN'S STRATEGY", description: "Board Room Boss", image: `${front}` },
-    { id: "7", title: "456 BIDDING WAR", description: "IPL AUCTION", image:`${ipl}` },
+    {
+      id: "1",
+      title: "DALGONA PAPERS",
+      description: "PAPER PRESENTATION",
+      image: `${paperpresent}`,
+    },
+    {
+      id: "2",
+      title: "GLASS BRIDGE DESIGN",
+      description: "UI/UX",
+      image: `${uiux}`,
+    },
+    {
+      id: "3",
+      title: "THE MASKED BUG",
+      description: "DEBUGGING",
+      image: `${debug}`,
+    },
+    {
+      id: "4",
+      title: "THE ELIMINATION ROOM",
+      description: "THE GLITCH ESCAPE",
+      image: `${Escape}`,
+    },
+    {
+      id: "5",
+      title: "DIGITAL DEATH MATCH",
+      description: "Mobile Games",
+      image: `${mobliegame}`,
+    },
+    {
+      id: "6",
+      title: "THE FRONT MAN'S STRATEGY",
+      description: "Board Room Boss",
+      image: `${front}`,
+    },
+    {
+      id: "7",
+      title: "456 BIDDING WAR",
+      description: "IPL AUCTION",
+      image: `${ipl}`,
+    },
   ];
 
   const settings = {
@@ -33,40 +70,76 @@ export default function Motion() {
     arrows: true,
     centerMode: true,
     centerPadding: "20%",
-    responsive:[
+    responsive: [
       {
-        breakpoint:640,
-        setting:{
-          centerpadding:"10%",
+        breakpoint: 640,
+        setting: {
+          centerpadding: "10%",
         },
       },
-    ]
+    ],
   };
 
   return (
     <>
-      <section className="relative w-full h-screen flex items-center justify-center">
+      <section className="relative w-full h-screen flex flex-col items-center justify-center">
+
         <video
           src={bgvideo}
           autoPlay
           loop
           muted
-          className="absolute top-0 left-0 w-full h-[100vh] object-cover md:object-contain"
+          className="absolute top-0 left-0 w-full h-full object-cover"
         />
         <h1 className="text-6xl md:text-8xl font-bold font-squid text-white text-center z-10">
           AVINYA'25
         </h1>
+        <div
+          className="absolute bottom-10 animate-bounce cursor-pointer"
+          onClick={() =>
+            document
+              .getElementById("descriptionSection")
+              .scrollIntoView({ behavior: "smooth" })
+          }
+        >
+          <svg
+            className="w-10 h-10 text-white"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </div>
       </section>
-      <section className="w-full min-h-screen bg-gray-900 flex flex-col items-center justify-center py-20 font-squid">
+
+      <section className="w-full min-h-screen bg-gray-900 flex flex-col items-center justify-center py-20 font-squid" style={{ backgroundImage: `url(${bg})`,    backgroundSize: "cover",    // Ensures the image covers the entire section
+    backgroundPosition: "center", 
+    backgroundRepeat: "no-repeat",
+    height: "100vh", }}>
         <h2 className="text-4xl font-bold text-white mb-10">Events</h2>
-        <div className="relative md:w-full w-80 max-w-lg">
+        <div className="relative md:w-full w-80 max-w-lg h-200 ">
           <Slider {...settings}>
             {events.map((event) => (
               <div key={event.id} className="flex justify-center">
                 <div className="relative flex flex-col items-center bg-gray-800 bg-opacity-50 rounded-lg p-6 w-50 shadow-lg sm:w-80">
-                  <img src={event.image} alt={event.title} className="w-25 h-25 mb-4 object-cover rounded-lg" />
-                  <h2 className="text-xs md:text-lg  font-bold text-white text-center">{event.title}</h2>
-                  <p className="text-sm text-gray-300 text-center">{event.description}</p>
+                  <img
+                    src={event.image}
+                    alt={event.title}
+                    className="w-25 h-25 mb-4 object-cover rounded-lg"
+                  />
+                  <h2 className="text-xs md:text-lg  font-bold text-white text-center">
+                    {event.title}
+                  </h2>
+                  <p className="text-sm text-gray-300 text-center">
+                    {event.description}
+                  </p>
                   <button
                     className="mt-4 px-6 py-2 text-xs md:text-lg bg-gray-700 text-white font-semibold border border-gray-500 rounded-md hover:bg-gray-600 transition cursor-pointer"
                     onClick={() => navigate(`/event/${event.id}`)}
